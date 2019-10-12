cf3c2724812219bb7e69dda761e7f0fc601bff11
Jeremy Kwa <jeremykwa@gmail.com>
2019-10-12 16:05:48 -0700

    get api call working & display to screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import { GOOGLE_PLACES_API_KEY } from 'react-native-dotenv';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+const JOKE_URL = 'http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe';
 
 export default function App() {
+  const [data, setJoke] = useState({ joke: '', isFetching: false });
+
+  useEffect(() => {
+    setJoke({...this.state, isFetching: true});
+    axios.get(JOKE_URL)
+      .then(response => {
+        setJoke({joke: response.data.value.joke, isFetching: false})
+      })
+      .catch(e => {
+        console.log(e);
+        setJoke({...data, isFetching: false});
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>{GOOGLE_PLACES_API_KEY}</Text>
+      <Text>{data.joke}</Text>
     </View>
   );
 }
