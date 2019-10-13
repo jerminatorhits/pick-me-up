7972f6c14778af648a61c6a58257b65b78650954
Jeremy Kwa <jeremykwa@gmail.com>
2019-10-12 19:33:57 -0700

    move App.tsx to src folder

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { GOOGLE_PLACES_API_KEY } from 'react-native-dotenv';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+const JOKE_URL = 'http://api.icndb.com/jokes/random';
+
+export default function App() {
+  const [data, setJoke] = useState({ joke: '', isFetching: false });
+
+  useEffect(() => {
+    setJoke({...this.state, isFetching: true});
+    axios.get(JOKE_URL)
+      .then(response => {
+        setJoke({joke: response.data.value.joke, isFetching: false})
+      })
+      .catch(e => {
+        console.log(e);
+        setJoke({...data, isFetching: false});
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {/* <Header title="Welcome to React Native"/> */}
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{data.joke}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
